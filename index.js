c0be8bfc5842f458020bee15b2ace848fa8b6a1f
YesYourShin <ehrl827@naver.com>
2022-10-26 15:41:44 +0900

    animeData

---
+++ index.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+const animeData = async () => {
+  // 온나다, 라프텔 등 또는 github api 사용해서 정보 가져오기
+  // const response = await axios.get('https://laftel.net/api/v1.0/items/16004/detail/');
+  // for (const data of response.) {
+  //   console.log(data);
+  // }
+};
+
+animeData();
